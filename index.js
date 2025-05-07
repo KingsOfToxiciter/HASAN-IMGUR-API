293726b7ca52a8afec726b991f3684f11c9687d2
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-05-07 19:35:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 
 app.get("/imgur", async (req, res) => {
     try {
